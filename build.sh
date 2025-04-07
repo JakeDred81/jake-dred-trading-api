b3a756833e30b7fb1d006e6223c9cbc0a60a96cf
Jake Dred <JakeDred81@example.com>
2025-04-07 17:56:10 -0500

    Add custom build and start scripts for Render

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Use the right pip
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Run your Flask app with gunicorn
+gunicorn main:app
